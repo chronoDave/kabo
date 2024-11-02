7b4d55af47857fa002335bf7e6c4d31f4987251c
chronoDave <davidwolters6758@gmail.com>
2024-11-02 23:50:46 +0000

    Added board actions tests

--- src/store/actions/board.ts
+++ src/store/actions/board.ts
@@ -1,41 +1,28 @@
-import type Store from '../../lib/store/store';
 import type { State } from '../state';
 import type { Board } from '../entities';
 
-import { produce } from 'immer';
-import uid from '../../lib/uid/uid';
+import type { Draft } from 'immer';
 
-export const create = (store: Store<State>) =>
-  (title: string): void => {
-    store.set(produce(draft => {
-      const id = uid();
-
-      draft.entity.board[id] = { id, title, lanes: [] };
-      draft.active.board = id;
-    }));
+export const create = (board: Board) =>
+  (draft: Draft<State>) => {
+    draft.entity.board[board.id] = board;
   };
 
-
-export const update = (store: Store<State>) =>
-  (id: string) =>
-    (board: Partial<Omit<Board, 'id'>>): void => {
-      store.set(produce(draft => {
-        if (typeof board.title === 'string') {
-          draft.entity.board[id].title = board.title.trim();
-        }
-      }));
+export const setTitle = (id: string) =>
+  (title: string | null) =>
+    (draft: Draft<State>) => {
+      draft.entity.board[id].title = title;
     };
 
-export const remove = (store: Store<State>) =>
-  (id: string): void => {
-    store.set(produce(draft => {
-      delete draft.entity.board[id];
-
-      const keys = Object.keys(draft.entity.board);
-      if (keys.length > 0) {
-        draft.active.board = keys[keys.length - 1];
-      } else {
-        delete draft.active.board;
+export const addLane = (id: string) =>
+  (lane: string) =>
+    (draft: Draft<State>) => {
+      if (!draft.entity.board[id].lanes.includes(lane)) {
+        draft.entity.board[id].lanes.push(lane);
       }
-    }));
+    };
+
+export const remove = (id: string) =>
+  (draft: Draft<State>) => {
+    delete draft.entity.board[id];
   };
