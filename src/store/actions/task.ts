7e2fb47c023ee83af4da163313ead31faba5e79a
chronoDave <davidwolters6758@gmail.com>
2024-11-03 14:18:40 +0000

    Added task unit tests

--- src/store/actions/task.ts
+++ src/store/actions/task.ts
@@ -1,45 +1,25 @@
-import type Store from '../../lib/store/store';
+import type { Draft } from 'immer';
 import type { Task } from '../entities';
 import type { State } from '../state';
 
-import { produce } from 'immer';
-import uid from '../../lib/uid/uid';
+export const create = (task: Task) =>
+  (draft: Draft<State>) => {
+    draft.entity.task[task.id] = task;
+  };
 
-export const create = (store: Store<State>) =>
-  (card: string) =>
-    (title: string): void => {
-      const id = uid();
-
-      store.set(produce(draft => {
-        draft.entity.task[id] = { id, title };
-        draft.entity.card[card].tasks.push(id);
-      }));
+export const setTitle = (id: string) =>
+  (title: string | null) =>
+    (draft: Draft<State>) => {
+      draft.entity.task[id].title = title;
     };
 
-export const update = (store: Store<State>) =>
-  (id: string) =>
-    (task: Partial<Omit<Task, 'id'>>): void => {
-      store.set(produce(draft => {
-        if (typeof task.title === 'string') {
-          draft.entity.task[id].title = task.title;
-        }
-
-        if (typeof task.done === 'boolean') {
-          draft.entity.task[id].done = task.done;
-        }
-      }));
+export const setDone = (id: string) =>
+  (done: boolean) =>
+    (draft: Draft<State>) => {
+      draft.entity.task[id].done = done;
     };
 
-export const remove = (store: Store<State>) =>
-  (id: string): void => {
-    store.set(produce(draft => {
-      delete draft.entity.task[id];
-
-      Object.values(draft.entity.card)
-        .filter(card => card.tasks.includes(id))
-        .forEach(card => {
-          const i = draft.entity.card[card.id].tasks.indexOf(id);
-          draft.entity.card[card.id].tasks.splice(i, 1);
-        });
-    }));
+export const remove = (id: string) =>
+  (draft: Draft<State>) => {
+    delete draft.entity.task[id];
   };
